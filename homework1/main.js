8133b98301016932c8bcf42c9cf10386b2dfa585
swswgumenyuk <swgumenyuk@gmail.com>
2022-02-03 21:17:07 +0200

    дз1

--- homework1/main.js
+++ homework1/main.js
@@ -5,41 +5,143 @@
 // console.log(a);
 // alert(a);
 // document.write(a);
+
 // let b = 'owu';
 // console.log(b);
 // alert(b);
 // document.write(b);
-// let c = 'com';
-// console.log(c);
-// alert(c);
-// document.write(c);
-let d = 'ua';
-console.log(d);
-alert(d);
-document.write(d);
-// let c = ;
-// console.log(c);
-// alert(c);
-// document.write(c);
-// let c = 'com';
-// console.log(c);
-// alert(c);
-// document.write(c);
+
 // let c = 'com';
 // console.log(c);
 // alert(c);
 // document.write(c);
 
+// let d = 'ua';
+// console.log(d);
+// alert(d);
+// document.write(d);
 
+// let e = 1;
+// console.log(e);
+// alert(e);
+// document.write(e);
 
+// let f = 10;
+// console.log(f);
+// alert(f);
+// document.write(f);
 
+// let x = -999;
+// console.log(x);
+// alert(x);
+// document.write(x);
 
+// let y = 123;
+// console.log(y);
+// alert(y);
+// document.write(y);
+
+// let pi = 3.14;
+// console.log(pi);
+// alert(pi);
+// document.write(pi);
+
+// let s = 2.7;
+// console.log(s);
+// alert(s);
+// document.write(s);
+
+// let g = 16;
+// console.log(g);
+// alert(g);
+// document.write(g);
+
+// let str = "true";
+// console.log(str);
+// alert(str);
+// document.write(str);
+
+// let str1 = 'false';
+// console.log(str1);
+// alert(str1);
+// document.write(str1);
 
 
 
 // - Переприсвоїти кожній змінній з завдання значення на довільне.
 //     Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+// let a = 'hi';
+// console.log(a);
+// alert(a);
+// document.write(a);
+
+// let b = 'map';
+// console.log(b);
+// alert(b);
+// document.write(b);
+
+// let c = 'abc';
+// console.log(c);
+// alert(c);
+// document.write(c);
+
+// let d = 'usa';
+// console.log(d);
+// alert(d);
+// document.write(d);
+
+// let e = 5;
+// console.log(e);
+// alert(e);
+// document.write(e);
+
+// let f = 22;
+// console.log(f);
+// alert(f);
+// document.write(f);
+
+// let x = -111;
+// console.log(x);
+// alert(x);
+// document.write(x);
+
+// let y = 587;
+// console.log(y);
+// alert(y);
+// document.write(y);
+
+// let pi = 2.12;
+// console.log(pi);
+// alert(pi);
+// document.write(pi);
+
+// let s = 3.5;
+// console.log(s);
+// alert(s);
+// document.write(s);
+
+// let g = 54;
+// console.log(g);
+// alert(g);
+// document.write(g);
+
+// let str = "NaN";
+// console.log(str);
+// alert(str);
+// document.write(str);
+
+// let str1 = 'true';
+// console.log(str1);
+// alert(str1);
+// document.write(str1);
+
+
 // - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+let firstName = "Gumenyuk";
+let middleName = "Switlana";
+let lastName = "Mykolaiwna"
+
 // - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
 //     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
 // - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
