b6db2de095d1d134b76e9baae5d0f55ad1016681
swswgumenyuk <swgumenyuk@gmail.com>
2022-02-05 19:40:14 +0200

    class and home work 1

--- homework1/main.js
+++ homework1/main.js
@@ -138,14 +138,36 @@
 
 
 // - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
-let firstName = "Gumenyuk";
-let middleName = "Switlana";
-let lastName = "Mykolaiwna"
+// let firstName = "Gumenyuk";
+// let middleName = "Switlana";
+// let lastName = "Mykolaiwna";
+// let person = 'Gumenyuk  Switlana  Mykolaiwna';
+// document.write(person);
+// console.log(person);
+
 
 // - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
 //     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+// const name = "Switlana";
+// let fn = 'Mycolaiwna'
+// let age = 35;
+//
+// let userName = prompt('What is your name?');
+// let userFn = prompt('What is your father name');
+// let userAge = prompt('How old are you?');
+//
+// document.write('Hello my name is ' + name + ' and I am ' + age + ' years old');
+
+
 // - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
-//     let a = 100; let b = '100'; let c = true;
+//     let a = 100;
+//     let b = '100';
+//     let c = true;
+//     console.log(typeof a);
+//     console.log(typeof b);
+//     console.log(typeof c);
+
+
 // - Визначити відповідний оператор в виразах що б вийшов відповідний результат.
 //     В однакових виразах не використовувати однакові оператори!!!
 //     5 ? 6 -> true
@@ -159,11 +181,117 @@ let lastName = "Mykolaiwna"
 // 10 ? 10 -> false
 // 123 ? '123' -> false
 // 123 ? '123' -> true
-//
+
+// console.log(5<6);
+// console.log(5>6);
+// console.log(5===6);
+// console.log(5>=6);
+// console.log(10===10);
+// console.log(10<10);
+// console.log(10>10);
+// console.log(10!=10);
+// console.log(10!==10);
+// console.log(123==='123');
+// console.log(123=='123');
+
+
 // - Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
 //     let str = "20";
-// let a = 5;
-// document.write(str + a + "<br/>");
-// document.write(str - a + "<br/>");
-// document.write(str * "2" + "<br/>");
-// document.write(str / 2 + "<br/>");
+//     let a = 5;
+// document.write(str + a + "<br/>");-> 205
+// document.write(str - a + "<br/>"); ->15
+// document.write(str * "2" + "<br/>"); -> 40
+// document.write(str / 2 + "<br/>");  ->10
+
+
+
+// Classwork
+// 1. Створити масив з 10 чисел (можна використовувати Math.random Та Math.floor). Скласти результат цих чисел в змінній result.
+// let numbers = [1, 5, 2, 7,44,12,8,64,28,32];
+// let result = 1+5+2+7+44+12+8+64+28+32;
+// console.log(result);
+
+// 2. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр
+// let book = {
+//     name: 'abc',
+//     number: 120,
+//     genre: 'comedy',
+// };
+// console.log(book);
+
+// 3. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр, автори
+// let book1 = {
+//     name: 'abc',
+//     number: 120,
+//     genre: 'comedy',
+//     author: 'kuk',
+// };
+// console.log(book1);
+
+// 4. Створити маис об'єктів book з наступними полями  - назва, кількість сторінок, жанр, автори. Вивести кожну книгу як окремий об'єкт
+// let books = [
+//     {name: 'abc', number: 120, genre: 'comedy', author: 'kuk',},
+//     {name: 'aaa', number: 150, genre: 'tragedy', author: 'ser',},
+//     {name: 'xxx', number: 220, genre: 'fantasy', author: 'tik',},
+//     {name: 'zzz', number: 555, genre: 'tale', author: 'mask',},
+//     {name: 'yyy', number: 333, genre: 'comedy', author: 'rava',},
+// ];
+// console.log(books[0]);
+// console.log(books[1]);
+// console.log(books[2]);
+// console.log(books[3]);
+// console.log(books[4]);
+
+// 5. Напишіть код, який вираховує площу прямокутника висотою 23см. (змінна height) та шириною 10см (змінна width).
+// Значення площі зберігати в змінній s.
+// let height = 23;
+// let width = 10;
+// let s = height*width;
+// console.log(s);
+
+// чи так?
+//
+// let width = +prompt('width');
+// let height = +prompt('height');
+// let s = width * height;
+// console.log(s);
+
+
+// 6. Напиши код, який знаходить об'єм циліндра заввишки 10м (змінна heightC) та діаметром основи 4м (dC),
+// результат помістіть у змінну v.
+// let heightC = 10;
+// let rC = 2;
+// let PI = 3.14;
+// let a = 2;
+// let rC2 = Math.pow(rC, a);
+// let V = PI * rC2 * heightC;
+// console.log(V);
+
+// або
+//
+// let heightC = +prompt('heightC');
+// let rC = +prompt('rC');
+// let a = 2;
+// let PI = parseFloat(prompt("PI"));
+// let rC2 = Math.pow(rC, a);
+// let V = PI * rC2 * heightC;
+// console.log(V);
+
+
+// 7. У прямокутного трикутника дві сторони n (зі значенням 3) та m (зі значенням 4).
+// Знайдіть гіпотенузу k за теоремою Піфагора (потрібно використовувати функцію Math.pow (число, ступінь) або оператор зведення в ступінь **).
+// let n = 3;
+// let m = 4;
+// let a = 2;
+// let n2 = Math.pow(n, a);
+// let m2 = Math.pow(m, a);
+// let k = Math.sqrt(n2 + m2);
+// console.log(k);
+//
+// або
+
+let n = +prompt('n');
+let m = +prompt('m');
+let a = 2;
+let k = Math.sqrt((Math.pow(n, a) + Math.pow(m, a)));
+console.log(k);
